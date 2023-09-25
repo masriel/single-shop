fa5bdcf6c785e1cf85a2c08a4443f89ea5e41adb
Ekaterina <muhinakaty2004@gmail.com>
2023-09-25 17:12:10 +0300

    login page

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Log In</title>
+    <link rel="stylesheet" href="css/auth.css">
+</head>
+<body>
+    <form class="form">
+        <h1 class="title">Log In</h1>
+
+        <div class="group">
+            <input class="input" placeholder=" ">
+            <label class="label">Login</label>
+        </div>
+
+        <div class="group">
+            <input class="input" placeholder=" ">
+            <label class="label">Password</label>
+        </div>
+
+        <button class="loginbut">Log in</button>
+    </form>
+</body>
+</html>
